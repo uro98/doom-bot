6da00ed66210a4c0f52f29109a25b9c4ec10759c
Yu Jo <zoey5538@gmail.com>
2018-03-11 02:49:59 +0000

    Add Json attributes

--- getInfo.py
+++ getInfo.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("http://localhost:6001/api/player")
+response = requests.get("http://localhost:6001/api/players")
 
 print(response.status_code)
 print(response.json())
